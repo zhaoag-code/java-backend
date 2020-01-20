2ac6ec17bcb724e6827f04ceed0f577e210aa7e5
Zag <zhaoangang666@163.com>
2020-01-20 09:50:07 +0800

    初始化项目

---
+++ src/main/java/io/backend/BackendApplication.java
@@ -0,0 +1,22 @@
+/**
+ * Copyright (c) 2016-2019 人人开源 All rights reserved.
+ *
+ * https://www.renren.io
+ *
+ * 版权所有，侵权必究！
+ */
+
+package io.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class BackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendApplication.class, args);
+	}
+
+}
